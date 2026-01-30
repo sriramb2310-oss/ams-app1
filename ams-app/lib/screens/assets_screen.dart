32e2a5d3747d78b2dca3472d7d05c290a05fec9a
sriramb2310-oss <sriramb2310@gmail.com>
2026-01-30 13:08:05 +0530

    Update assets_screen.dart

--- ams-app/lib/screens/assets_screen.dart
+++ ams-app/lib/screens/assets_screen.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AssetsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Assets')),
+      body: Center(child: Text('Assets Screen')),
+    );
+  }
+}
 
